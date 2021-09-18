6e6f727082160f0b10d6807021eec023e3220b7a
Jason Crockett <JesusisLord33333@outlook.com>
2021-09-18 18:19:10 +0100

    Initial commit - fleshed out most of the functions in the classes

---
+++ main.py
@@ -0,0 +1,31 @@
+# coded with the help of angels
+
+
+
+class Book:
+    def __init__(self,name):
+        pass
+
+    #  Takes a number and converts it into alphabetic form
+    def _convert_number_into_alpha(self,number :int):
+        pass
+
+    def _create_dialogue_file(self):
+        pass
+
+    def create_book(self):
+        pass
+
+
+class Bible:
+
+    def __init__(self, book_list):
+        self.book_list = book_list
+
+    def create_all_dialogue_files(self):
+        pass
+
+if __name__ == '__main__':
+    list = open('./books.list', 'r').read()
+
+
